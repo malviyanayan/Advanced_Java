bd6af4e38562d26cce2b571a3739d2a2d9791bb8
nayan malviya <naymalviya@gmail.com>
2024-11-17 22:46:13 +0530

    an application which perform general CRUD opration with full stack

---
+++ x_jdbc/WEB-INF/src/servlets/SignUpServlet.java
@@ -0,0 +1,39 @@
+package servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Date;
+import java.sql.SQLException;
+
+public class SignUpServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String name = request.getParameter("unm");
+        String email = request.getParameter("eml");
+        String password = request.getParameter("pwd");
+        String dob = request.getParameter("dob");
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/?user=root&password=1234");
+
+            String query = "insert into students (name, email, password, dob) value (?, ?, ?, ?)";
+
+            PreparedStatement ps = con.prepareStatement(query);
+
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setString(3, password);
+            Date dateOfBirth = Date.valueOf(dob);
+            ps.setDate(4, dateOfBirth);
+
+            ps.executeUpdate();
+        } catch(ClassNotFoundException|SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
